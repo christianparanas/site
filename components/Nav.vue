fead3147273c163ca09ac99caceb1d987b2d6ceb
Christian Paranas <christiannparanas@gmail.com>
2020-11-25 23:29:22 +0800

    pre nav

---
+++ components/Nav.vue
@@ -0,0 +1,9 @@
+<template>
+   <div class="container shadow flex p-4">
+      <div class="flex-1">chrisparams</div>
+      <div class="flex-initial px-5">menu</div>
+      <div class="flex-initial w-10 p-1 border-4 rounded-lg">
+         <Light />
+      </div>
+   </div>
+</template>
