ed1dc3304556937a978fefa7e532ed676082042c
Christian Paranas <christiannparanas@gmail.com>
2020-11-26 19:41:19 +0800

    init nav

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,9 +1,24 @@
 <template>
-   <div class="container shadow flex p-4">
-      <div class="flex-1">chrisparams</div>
-      <div class="flex-initial px-5">menu</div>
-      <div class="flex-initial w-10 p-1 border-4 rounded-lg">
-         <Light />
+   <div class="flex p-4">
+      <div class="flex-1 flex">
+         <div class="flex-initial w-10 p-2 cursor-pointer">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#747A87">
+               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
+            </svg>
+         </div>
+         <div class="py-2 text-gray-600">
+            christianparanas
+         </div>
+      </div>
+      <div class="flex-initial w-10 p-1 border-4 border-transparent rounded-lg hover:border-gray-400 transition duration-400 ease-in-out cursor-pointer">
+         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#747A87">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h16M4 16h16" />
+         </svg>
+      </div>
+      <div class="flex-initial w-10 p-1 border-4 border-transparent rounded-lg hover:border-gray-400 transition duration-400 ease-in-out cursor-pointer">
+         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#747A87">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+         </svg>
       </div>
    </div>
 </template>
