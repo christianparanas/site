188454d3e1df8b804216128712bdca446d3fc7ef
Christian Paranas <christiannparanas@gmail.com>
2020-11-26 23:33:23 +0800

    hero

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,24 +1,36 @@
 <template>
-   <div class="flex p-4">
+   <div class="nav shadow-sm p-4 flex">
       <div class="flex-1 flex">
          <div class="flex-initial w-10 p-2 cursor-pointer">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#747A87">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#ffffff">
                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4" />
             </svg>
          </div>
-         <div class="py-2 text-gray-600">
+         <div class="py-2 text-white">
             christianparanas
          </div>
       </div>
-      <div class="flex-initial w-10 p-1 border-4 border-transparent rounded-lg hover:border-gray-400 transition duration-400 ease-in-out cursor-pointer">
-         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#747A87">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 8h16M4 16h16" />
+      <div class="flex-initial w-10 p-1 mx-2 border-4 border-transparent cursor-pointer">
+         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#ffffff">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M4 8h16M4 16h16" />
          </svg>
       </div>
-      <div class="flex-initial w-10 p-1 border-4 border-transparent rounded-lg hover:border-gray-400 transition duration-400 ease-in-out cursor-pointer">
-         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="#747A87">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
+      <div class="dark flex-initial w-10 p-1 border-4 border-transparent rounded-full cursor-pointer">
+         <svg class="dark-svg w-6 h-6" fill="#ffffff" stroke="#ffffff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path>
          </svg>
       </div>
    </div>
 </template>
+
+<style scoped>
+   .nav {
+      background-color: #7000DF;
+      width: 100%;
+      position: fixed;
+   }
+
+   .dark:hover {
+      background-color: #9061f9;
+   }
+   
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,29 @@
 <template>
-  <div class="">
-    <Nav />
+  <div class="main">
+    <div class="hero">
+      <Nav />
+    </div>
+    <div class="try">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, sint? Eius facere eum nihil cupiditate magnam nostrum, minus aspernatur non obcaecati voluptatum earum perferendis assumenda quae ipsam ducimus modi harum rerum similique quisquam animi molestias. Cupiditate dignissimos obcaecati dolor maiores mollitia quas voluptatum? Nesciunt obcaecati, ut in fugit itaque dolore libero autem? Quia, error aliquid, iusto vero magni minima, dignissimos nihil expedita cupiditate cumque eaque doloremque. Quaerat ullam laudantium dicta dignissimos nihil voluptas dolorem natus? Explicabo quia ab non nostrum quaerat, mollitia doloremque nobis beatae temporibus enim voluptatem labore iste optio deleniti excepturi quis quae sit ut quos earum. Doloremque.
+    </div>
   </div>
 </template>
 
 <script>
 export default {}
 </script>
+
+<style scoped>
+  .main {
+    background-color: #7000DF;
+    height: 100%;
+  }
+
+  .hero {
+    background-color: #7000DF;
+    height: 70vh;
+  }
+  .try {
+    background-color: #ffffff;
+  }
+</style>
