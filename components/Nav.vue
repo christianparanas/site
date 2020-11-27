a4cb45ac376f1dd480a6f37fc98d7c6b535ebb04
Christian Paranas <christiannparanas@gmail.com>
2020-11-27 19:04:19 +0800

    hero

--- components/Nav.vue
+++ components/Nav.vue
@@ -16,7 +16,7 @@
       <div v-if="toggleMode" @click="$colorMode.preference = 'light'; color = 'light'; toggleMode = !toggleMode" class="flex-initial w-10 p-1 border-4 border-transparent rounded-full cursor-pointer">
          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
       </div>
-      <div v-else @click="$colorMode.preference = 'dark'; btnLabel; color = 'dark'; toggleMode = !toggleMode" class="flex-initial w-10 p-1 border-4 border-transparent rounded-full cursor-pointer">
+      <div v-else @click="$colorMode.preference = 'dark'; color = 'dark'; toggleMode = !toggleMode" class="flex-initial w-10 p-1 border-4 border-transparent rounded-full cursor-pointer">
          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"></path></svg>
       </div>
    </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="App" class="main">
+    <Nav />
     <div class="hero">
-      <Nav />
+      <h2>Hi! I'm</h2>
+      <h1>Christian R. Paranas</h1>
+      <p>I'm a Student and a Freelance Web Developer currently in Tacloban City, Philippines.</p>
     </div>
   </div>
 </template>
@@ -11,6 +14,19 @@
 <style scoped>
   .main {
     height: 100%;
+    font-family: 'Fredoka One', cursive;
+  }
+
+  .hero {
+    padding: 150px 20px 25px;
+  }
+
+  h2 {
+    font-size: 15px;
+  }
+
+  h1 {
+    font-size: 35px;
   }
 
 </style>
