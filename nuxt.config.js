106def94f96fe2852b0aa5a584d7fc148fb7a441
Christian Paranas <christiannparanas@gmail.com>
2020-11-28 01:07:40 +0800

    links

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: "I'm a Student and a Freelance Web Developer currently in Tacloban City, Philippines." }
+      { hid: 'description', name: 'description', content: "I'm Christian R. Paranas, I am a Student and a Freelance Web Developer currently in Tacloban City, Philippines." }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
