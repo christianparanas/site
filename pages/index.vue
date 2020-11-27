ffb4c6fb98cfaef40e19f144f59436e7e964ecc6
Christian Paranas <christiannparanas@gmail.com>
2020-11-27 23:28:04 +0800

    icons

--- pages/index.vue
+++ pages/index.vue
@@ -6,12 +6,16 @@
       <h1>Christian R. Paranas</h1>
       <p>I'm a Student and a Freelance Web Developer currently in Tacloban City, Philippines.</p>
       <div class="options">
-        <div class="pr">Projects</div>
-        <div class="cv">Curriculum Vitae</div>
+        <div class="pr shadow-md">
+          <div class="text">Projects</div>
+          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 20l4-16m4 4l4 4-4 4M6 16l-4-4 4-4"></path></svg>
+        </div>
+        <div class="cv shadow-md">
+          <div class="text">Curriculum Vitae</div>
+          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"></path></svg>
+        </div>
       </div>
-      <div class="footer">
-        <div class="cre">2020 &copy Christian Paranas</div>
-      </div>
+      <Footer />
     </div>
   </div>
 </template>
@@ -30,21 +34,27 @@ export default {
     font-family: 'Fredoka One', cursive;
   }
 
-  .hero { padding: 180px 20px 25px; }
+  .hero { padding: 150px 20px 25px; }
 
   h2 { font-size: 15px; }
-  h1 { font-size: 35px; }
+  h1 { font-size: 30px; }
 
   .options {
     display: grid;
-    grid-row-gap: 12px;
+    grid-row-gap: 10px;
     text-align: center;
-    margin-top: 40px;
+    margin-top: 25px;
   }
 
   .pr, .cv {
-    padding: 15px;
+    padding: 12px;
     border-radius: 6px;
+    display: flex;
+    justify-content: center;
+  }
+
+  .text {
+    padding-right: 6px;
   }
 
   .pr {
@@ -55,15 +65,6 @@ export default {
   .cv {
     background-color: var(--hero2bg);
     color: var(--hero2color);
-  }
-
-  .footer {
-    position: fixed;
-    bottom: 0;
-  }
-
-  .cre {
-    font-size: 15px;
   }
 
 </style>
