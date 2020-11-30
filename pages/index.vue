a348c0a2fb40f0b46d57ebfa161ac19f85ac40d8
Christian Paranas <christiannparanas@gmail.com>
2020-11-30 18:12:15 +0800

    can

--- pages/index.vue
+++ pages/index.vue
@@ -15,6 +15,40 @@
           <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 13l-3 3m0 0l-3-3m3 3V8m0 13a9 9 0 110-18 9 9 0 010 18z"></path></svg>
         </a>
       </div>
+      <div class="can">
+        <div class="shadow dev">
+            <div class="svg">
+              <svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128"><path class="st0 cls-1" d="M105.28513,5.09767a3.68385,3.68385,0,0,0-3.09682-1.74645,3.62149,3.62149,0,0,0-3.14343,1.7341c-1.14877,1.84741-2.959,4.04954-5.05954,4.04954H35.58658a13.61691,13.61691,0,0,0-9.8803,4.232A12.8741,12.8741,0,0,0,22.18289,23.045,13.13794,13.13794,0,0,0,35.28862,35.3924H52.66079a4.96144,4.96144,0,0,1,4.95586,4.95576V51.45168A25.07443,25.07443,0,0,0,35.70389,73.47543L32.75043,91.98326a28.22029,28.22029,0,1,0,55.73585,0L85.53283,73.47543A25.06435,25.06435,0,0,0,64.176,51.52526V40.34816A11.52821,11.52821,0,0,0,52.66079,28.833H35.48593a6.62368,6.62368,0,0,1-6.7196-6.21648,6.53744,6.53744,0,0,1,6.52229-6.84276h58.7365a12.77547,12.77547,0,0,0,11.46579-7.26219A3.49815,3.49815,0,0,0,105.28513,5.09767ZM68.71728,73.45827v9.54884a8.09892,8.09892,0,0,1-16.19785,0V73.45827a8.11222,8.11222,0,0,1,5.95408-7.80936v-9.98a20.68088,20.68088,0,0,1,4.28968,0v9.98A8.11222,8.11222,0,0,1,68.71728,73.45827Z"/><path class="st9 cls-1" d="M57.23806,73.45831v9.54884a3.38027,3.38027,0,0,0,6.76054,0V73.45831a3.38027,3.38027,0,1,0-6.76054,0Z"/></svg>
+          </div>
+          <div class="cantitle">Web development</div>
+          <div class="candis">I can build your website from simple features to advance, front-end and back-end development.</div>
+        </div>
+        <div class="shadow dev">
+          <div class="svg">
+            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+              viewBox="0 0 512 512" xml:space="preserve">
+            <path class="st0" d="M115.2,499.2C86.9,499.2,64,476.3,64,448V64c0-28.3,22.9-51.2,51.2-51.2H302l146,146.1V448
+              c0,28.3-22.9,51.2-51.2,51.2H115.2z"/>
+            <g class="st1">
+              <path class="st2" d="M435.2,128L435.2,128l-88.5-88.5c-30.3,5.6-52.3,32.1-52.3,62.9V128c0,35.3,28.7,64,64,64h76.8V128z"/>
+            </g>
+            <path class="st0" d="M358.4,153.6c-28.3,0-51.2-22.9-51.2-51.2V18.1l135.1,135.5H358.4z"/>
+            <path class="st3" d="M307.3,0H115.2c-35.3,0-64,28.7-64,64v384c0,35.3,28.7,64,64,64h281.6c35.3,0,64-28.7,64-64V153.6L307.3,0z
+              M411.5,140.8h-53.1c-21.2,0-38.4-17.2-38.4-38.4V49.1L411.5,140.8z M435.2,448c0,21.2-17.2,38.4-38.4,38.4H115.2
+              c-21.2,0-38.4-17.2-38.4-38.4V64c0-21.2,17.2-38.4,38.4-38.4h179.2v76.8c0,35.3,28.7,64,64,64h76.8V448z"/>
+            <path class="st3" d="M201.8,290.2l-55.5,29.9c-4.2,2.5-5.5,4-5.5,8.5v8.3c0,4.5,1.3,5.8,5.5,8.3l55.5,30c5.2,2.7,7,1,7-5.1v-11.4
+              c0-7.1-1-8.5-6.2-11l-30.7-15v-0.8l30.9-15.1c4.9-2.5,6-3.8,6-11v-10.4C208.8,289.2,207,287.6,201.8,290.2z"/>
+            <path class="st3" d="M287.5,268.8h-13.4c-4.7,0-6.7,1.2-8.2,5.6l-43.8,116.7c-1.3,4-0.5,5.6,4,5.6h13.4c4.7,0,6.7-1.2,8.2-5.6
+              l43.8-116.7C293,270.5,292,268.8,287.5,268.8z"/>
+            <path class="st4" d="M303,295.2v11.3c0,7.1,1.2,8.5,6.4,11l30.7,15v0.8l-30.9,15.1c-4.8,2.5-6.2,4.1-6.2,11v10.8c0,6,2.3,7.6,7.2,5
+              l55.5-29.9c4.2-2.5,5.5-4.3,5.5-8.5v-8.3c0-4.1-1.3-5.8-5.5-8.3l-55.5-30C305.4,287.4,303,289,303,295.2z"/>
+            </svg>
+
+          </div>
+          <div class="cantitle">Web development</div>
+          <div class="candis">I can build your website from simple features to advance, front-end and back-end development.</div>
+        </div>
+      </div>
       <Footer />
     </div>
   </div>
@@ -22,36 +56,41 @@
 
 <script>
 export default {
-  head: {
-    title: 'Software Developer',
-    titleTemplate: "Christian Paranas | %s",
-    htmlAttrs: {
-      lang: 'en',
-      amp: true
-    },
-    meta: [
-      { name: 'keywords', content: 'Christian Paranas, Paranas Christian, Christian, Paranas, Christian R. Paranas, christianparanas, Web Developer, Student, Tacloban, Philippines'},
-      { hid: 'description', name: 'description', content: "I'm Christian R. Paranas, I'm a software developer currently in Tacloban City, Philippines. Specializing in building websites and applications. My current toolset includes Vue, React, Redux, Node, Laravel and other various frameworks, libraries and technologies related to them." },
-      { property: "og:title", content: "Christian Paranas | Software Developer" },
-      { property: "og:url", content: "https://christianparanas.me" },
-      { property: "og:site_name", content: "Christian Paranas | Software Developer" },
-      { property: "og:image", content: "https://og-image.vercel.app/christianparanas.me.png" },
-      { property: "og:type", content: "website" },
-      {
-        property: "og:description",
-        content: "Christian Paranas is a software developer currently in Tacloban City, Philippines. Specializing in building websites and applications."
-      }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  }
+  
 }
 </script>
 
 
 
 <style scoped>
+  .st9 {fill: #091a28}
+  .st0{fill:#4FD8BE;}
+  .st1{opacity:0.14;}
+  .st2{fill:#17292D;}
+  .st3{fill:#07342D;}
+  .st4{fill:#00372A;}
+
+  .can {
+    display: grid;
+    grid-row-gap: 10px;
+    margin-top: 50px;
+  }
+
+  .dev {
+    background-color: var(--menubg);
+    display: grid;
+    grid-template-columns: 90px 1fr;
+    padding: 20px;
+    text-align: center;
+    border-radius: 8px;
+    border: 3px solid #4FD8BE;
+  }
+
+  .svg {
+    grid-row: 1/3;
+  }
+
+
   .main {
     height: 100%;
     font-family: 'Fredoka One', cursive;
@@ -92,6 +131,29 @@ export default {
   .cv {
     background-color: var(--hero2bg);
     color: var(--hero2color);
+  }
+  /* 624px */
+  @media (min-width: 624px) {
+
+    .hero { 
+      width: 400px;
+      margin: 0 auto;
+      text-align: start;
+      padding: 150px 20px 25px; }
+
+    h2 { font-size: 15px; }
+    h1 { font-size: 30px; }
+
+    .options {
+      place-items: center;
+    }
+
+    .pr, .cv {
+      width: 400px;
+      padding: 12px;
+      display: flex;
+      justify-content: center;
+    }
   }
 
 </style>
