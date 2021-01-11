8c6b7438ae7840147aff8d84e4fa7d9a5cacb1b3
Yuri Orlov <christiannparanas@gmail.com>
2021-01-11 22:27:37 +0800

    disabled light mode

--- components/Nav.vue
+++ components/Nav.vue
@@ -13,7 +13,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M4 8h16M4 16h16" />
          </svg>
       </div>
-      <div v-if="toggleMode" @click="$colorMode.preference = 'light'; toggleMode = !toggleMode" class="menu-icon flex-initial w-10 p-1 border-4 border-transparent rounded-full cursor-pointer">
+      <div v-if="toggleMode" @click="$colorMode.preference = 'dark'; toggleMode = !toggleMode" class="menu-icon flex-initial w-10 p-1 border-4 border-transparent rounded-full cursor-pointer">
          <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
       </div>
       <div v-else @click="$colorMode.preference = 'dark'; toggleMode = !toggleMode" class="menu-icon flex-initial w-10 p-1 border-4 border-transparent rounded-full cursor-pointer">
